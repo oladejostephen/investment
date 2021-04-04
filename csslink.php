261dc27c3ddd5c7fa7f94bb888b2dacd76694d51
oladejostephen <oladejosteven@gmail.com>
2021-04-04 17:55:11 +0100

    mmm

---
+++ csslink.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from azim.commonsupport.com/Finandox/index-3.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 04 Apr 2021 15:12:02 GMT -->
+
+<head>
+    <meta charset="utf-8">
+    <title>Invest - Business HTML Template</title>
+    <!-- Stylesheets -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <!-- Responsive File -->
+    <link href="assets/css/responsive.css" rel="stylesheet">
+    <!-- Color File -->
+    <link href="assets/css/color-3.css" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&amp;display=swap" rel="stylesheet">
+
+    <link rel="shortcut icon" href="assets/images/favicon-3.png" type="image/x-icon">
+    <link rel="icon" href="assets/images/favicon-3.png" type="image/x-icon">
+
+    <!-- Responsive -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
+    <!--[if lt IE 9]><script src="js/respond.js"></script><![endif]-->
+</head>
---
+++ header.php
@@ -0,0 +1,150 @@
+<div class="loader-wrap style-three">
+            <div class="preloader">
+                <div class="preloader-close">Preloader Close</div>
+            </div>
+            <div class="layer layer-one"><span class="overlay"></span></div>
+            <div class="layer layer-two"><span class="overlay"></span></div>
+            <div class="layer layer-three"><span class="overlay"></span></div>
+        </div>
+
+        <!-- Main Header -->
+        <header class="main-header header-style-three">
+
+            <!-- Header Upper -->
+            <div class="header-upper">
+                <div class="auto-container">
+                    <div class="inner-container">
+                        <!--Nav Box-->
+                        <div class="nav-outer clearfix">
+                            <!--Logo-->
+                            <div class="logo-box">
+                                <div class="logo">
+                                    <a href="index.php"><img src="assets/images/logo.png" alt=""></a>
+                                </div>
+                            </div>
+                            <!--Sidemenu Navigation Toggler-->
+                            <div class="sidemenu-nav-toggler"><span class="icon fal fa-bars"></span></div>
+                            <!--Mobile Navigation Toggler-->
+                            <div class="mobile-nav-toggler"><span class="icon fal fa-bars"></span></div>
+
+                            <!-- Main Menu -->
+                            <nav class="main-menu navbar-expand-md navbar-light">
+                                <div class="collapse navbar-collapse show clearfix" id="navbarSupportedContent">
+                                    <ul class="navigation clearfix">
+                                        <li class="dropdown"><a href="index.php">Home</a>
+                                            
+                                        </li>
+                                        <li><a href="about.php">About Us</a></li>
+                                        <li class="dropdown"><a href="#">Services</a>
+                                            <ul>
+                                                <li><a href="services-1.php">Services One</a></li>
+                                                <li><a href="services-2.php">Services Two</a></li>
+                                                <li><a href="services-details.php">Services Details</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="dropdown"><a href="#">Portfolio</a>
+                                            <ul>
+                                                <li><a href="portfolio-1.php">Portfolio One</a></li>
+                                                <li><a href="portfolio-2.php">Portfolio Two</a></li>
+                                                <li><a href="portfolio-details.php">Portfolio Details</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="dropdown"><a href="#">News</a>
+                                            <ul>
+                                                <li><a href="blog-grid.php">Blog Grid</a></li>
+                                                <li><a href="blog-standard.php">Blog Standard</a></li>
+                                                <li><a href="blog-details.php">Blog Details</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="dropdown"><a href="#">Pages</a>
+                                            <ul>
+                                                <li><a href="careers.php">Careers</a></li>
+                                                <li><a href="team.php">Team</a></li>
+                                                <li><a href="team-details.php">Team Details</a></li>
+
+                                                <li class="dropdown"><a href="shop.php">Shop</a>
+                                                    <ul>
+                                                        <li><a href="shop.php">Products</a></li>
+                                                        <li><a href="product-details.php">Product Details</a></li>
+                                                        <li><a href="shopping-cart.php">Shopping Cart</a></li>
+                                                        <li><a href="checkout.php">Checkout</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li><a href="faq.php">Faq's</a></li>
+                                                <li><a href="404.php">404</a></li>
+                                            </ul>
+                                        </li>
+
+                                        <li><a href="contact.php">Contact</a></li>
+                                    </ul>
+                                </div>
+                            </nav>
+                            <!-- Main Menu End-->
+
+                            <!-- Header Right Content -->
+                            <div class="header-right-content clearfix">
+                                <div class="contact-info">
+                                    <div class="icon-box"><i class="flaticon-call-1"></i></div>
+                                    <div class="text">Phone Number</div>
+                                    <h4><a href="tel:+98787687676">+987 876 876 76</a></h4>
+                                </div>
+                                <!-- Link Btn-->
+                                <div class="link-btn"><a href="#" class="theme-btn btn-style-one"><span class="btn-title">Get A Quote</span></a></div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--End Header Upper-->
+
+            <!-- Sticky Header  -->
+            <div class="sticky-header">
+                <div class="auto-container clearfix">
+                    <!--Logo-->
+                    <div class="logo pull-left">
+                        <a href="index.php" title=""><img src="assets/images/sticky-logo-3.png" alt="" title=""></a>
+                    </div>
+                    <!--Right Col-->
+                    <div class="pull-right">
+                        <!-- Main Menu -->
+                        <nav class="main-menu clearfix">
+                            <!--Keep This Empty / Menu will come through Javascript-->
+                        </nav>
+                        <!-- Main Menu End-->
+                    </div>
+                </div>
+            </div>
+            <!-- End Sticky Menu -->
+
+            <!-- Mobile Menu  -->
+            <div class="mobile-menu">
+                <div class="menu-backdrop"></div>
+                <div class="close-btn"><span class="icon flaticon-cancel"></span></div>
+
+                <nav class="menu-box">
+                    <div class="nav-logo">
+                        <a href="index.php"><img src="assets/images/logo.png" alt="" title=""></a>
+                    </div>
+                    <div class="menu-outer">
+                        <!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header-->
+                    </div>
+                    <!--Social Links-->
+                    <div class="social-links">
+                        <ul class="clearfix">
+                            <li><a href="#"><span class="fab fa-twitter"></span></a></li>
+                            <li><a href="#"><span class="fab fa-facebook-square"></span></a></li>
+                            <li><a href="#"><span class="fab fa-pinterest-p"></span></a></li>
+                            <li><a href="#"><span class="fab fa-instagram"></span></a></li>
+                            <li><a href="#"><span class="fab fa-youtube"></span></a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+            <!-- End Mobile Menu -->
+
+            <div class="nav-overlay">
+                <div class="cursor"></div>
+                <div class="cursor-follower"></div>
+            </div>
+        </header>
